edec93ae75beafb67e9c831a0689d39782d06285
Trond Jakob Sjøvang <trond.sjovang@atea.no>
2022-07-06 12:02:33 +0200

    add example

---
+++ examples/minimal-module/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azuread" {}
+
+provider "azurerm" {
+  features {}
+}
+
+module "breakglass" {
+  source   = "sjovang/emergency-access-account/azuread"
+  password = var.password
+}
---
+++ examples/minimal-module/variables.tf
@@ -0,0 +1,5 @@
+variable "password" {
+  type        = string
+  sensitive   = true
+  description = "Password for the breakglass account"
+}
